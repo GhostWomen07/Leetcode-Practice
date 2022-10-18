673fa996b0f12786a62e6f22aafcca653d801e3b
Neha Negi <nehanegi071097@gmail.com>
2022-10-18 23:52:58 +0530

    first solution

--- TwoSum.js
+++ TwoSum.js
@@ -1,1 +1,24 @@
+// first solution
+
+const nums=[7,2,11,23];
+const target=9;
+
+var twoSum = function(nums, target) {
+   const res = {};
+    for(let i=0; i<=nums.length; i++){
+        if(res[nums[i]] >= 0){
+            return [res[nums[i]], i]
+        }
+        
+        res[target - nums[i]] = i;
+    }
+};
+
+console.log("first_solution",twoSum)
+
+//second_Solution
+
+
+
+
 
