c42022b5c5476d20df3af8f532ce2956a05eb581
Neha Negi <nehanegi071097@gmail.com>
2022-10-30 20:38:04 +0530

    Solution of longest Common Prefix

--- LongestCommonPrefix.js
+++ LongestCommonPrefix.js
@@ -1,3 +1,6 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string ""
+
 const strs=["misha", "miss","misti"]
 
 var longestCommonPrefix = function(strs) {
@@ -20,3 +23,19 @@ var longestCommonPrefix = function(strs) {
 };
 
 console.log(longestCommonPrefix(strs))
+
+### Explanation:-
+1. if strs is null or strs length is 0 then return blank string ("")
+2. then sort the string by used sort method strs.sort(), so we can only compare the first and last string of array 
+3. let i=0, used while loop check if i is smaller than the first string of array (i<strs[0].length),(0<5)it means condition true then 
+                 if(strs[0][i]!==strs[strs.length-1][i]){
+//                      if(strs[0][0]!==strs[3-1][0])
+//                          if(m!== strs[2][0])
+//                      if(m!==m)
+                             
+                     return strs[0].substr(0,i);}
+//                       misha.substr(0,3)
+//                        return mis  0 start, 3 is length
+                       i++;
+                    }
+if false then return strs[0]
